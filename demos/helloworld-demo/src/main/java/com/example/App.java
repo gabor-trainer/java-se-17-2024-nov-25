896a27309c849ea22379054062655c2e4b945460
github will ignore this <gabor.trainer@outlook.com>
2024-11-25 10:29:21 +0100

    feat[demos]: hello-world

---
+++ demos/helloworld-demo/src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        int x = 0;
+        System.out.println("Hello World!x");
+    }
+}
